5d42c09a3beae67336ded2aacd560b4c69495ef6
Alexander <english3000.org@gmail.com>
2018-01-07 01:52:13 -0800

    -

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+// import logo from './logo.svg';
+import './App.css';
+
+export default class App extends React.Component {
+  render() {
+    return ( <div>
+      <header style={{position: 'absolute', left: 0, right: 0, margin: 'auto'}}>
+        <p style={{textDecoration: 'underline', textUnderlinePosition: 'under', paddingTop: 30}}>Code 3000</p>
+      </header>
+      <p style={{color: 'black', textDecoration: 'underline', textUnderlinePosition: 'under', paddingTop: 30}}>Code 3000</p>
+      :)
+      {/* component */}
+    </div> );
+  }
+}
+/* <img src={logo} className="App-logo" alt="logo" /> */
